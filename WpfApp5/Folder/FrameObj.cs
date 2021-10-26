9ed61f4f44cb4dc24e459080b9163431cb644320
Александр Овчинников <shadow000fox@gmail.com>
2021-10-27 00:35:40 +0300

    Добавьте файлы проекта.

---
+++ WpfApp5/Folder/FrameObj.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp5.folder
+{
+    class FrameObj
+    {
+        public static Frame fRmN;
+    }
+}
---
+++ WpfApp5/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp5.Ado;
+using WpfApp5.folder;
+using WpfApp5.folder.Pages;
+
+namespace WpfApp5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Connect0db.Con0Db = new AgentEntity();
+            FrameObj.fRmN = FrMANE;
+            FrMANE.Navigate(new PageProduct());
+        }
+
+    }
+    
+}
